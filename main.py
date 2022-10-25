a4060b84f9dfda01bd08005268b20ab9e41c0481
Karnatour <vojtasokala@gmail.com>
2022-10-25 20:22:52 +0200

    add_config_for_token_and_host

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import lavaplayer
 import discord
+import json
 from discord.ext import commands
 
-TOKEN = 'MTAzMzgxMjYxNTQxNzMwNzE0Ng.Glil6t.pxijtX5-cAdJpTMEvEEq99LXwRog2fqSSkG1KQ'
+with open("config.json", "r") as file:
+    jsonData = json.load(file)
+
+TOKENjs = jsonData["token"]
+hostjs = jsonData["host"]
+
+TOKEN = TOKENjs
 PREFIX = "?"
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='?', intents=intents)
 
-
 lavalink = lavaplayer.LavalinkClient(
-    host="46.101.104.234",
+    host=hostjs,
     port=5464,
     password="votabot",
     user_id=1033812615417307146,
@@ -25,12 +31,6 @@ async def close():
         await guild.change_voice_state(channel=None)
 
 
-@bot.command(name="help", aliases=["commands"], help="Shows all commands.")
-async def help_commmand(ctx: commands.Context):
-    embed = discord.Embed(title="Help", description="Toto je Help command", color=0x00ff00)
-    embed.description = "\n".join(f"`{PREFIX}{command.name}`: {command.help}" for command in bot.commands)
-    await ctx.send(embed=embed)
-
 @bot.event
 async def on_ready():
     lavalink.set_user_id(bot.user.id)
@@ -38,10 +38,12 @@ async def on_ready():
     lavalink.connect()
     print("Lavalink je ready")
 
+
 @bot.command(help="Připoj se do kanálu")
 async def join(ctx: commands.Context):
     await ctx.guild.change_voice_state(channel=ctx.author.voice.channel)
     await lavalink.wait_for_connection(ctx.guild.id)
+
 
 @bot.command(name="Odpoj se z kanálu")
 async def leave(ctx: commands.Context):
